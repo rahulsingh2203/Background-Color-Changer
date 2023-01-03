c6a20c58560488bf896a34477ddfd602505b9ec0
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-03 11:00:32 +0530

    first commit-not mobile responsive

---
+++ practice.js
@@ -0,0 +1,18 @@
+function pink() {
+    document.body.style.backgroundColor = '#C35E9E';
+    document.getElementById("head").style.color = '#362360';
+};
+
+function blue() {
+    document.body.style.backgroundColor = '#84A6D3';
+    document.getElementById("head").style.color = '#362360';
+};
+function lavender() {
+    document.body.style.backgroundColor = '#C3AED9';
+    document.getElementById("head").style.color = '#362360';
+};
+
+function defaults(){
+    document.body.style.backgroundColor = '#362360';
+    document.getElementById("head").style.color = '#E9F3FB';
+}
